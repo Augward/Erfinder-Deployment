92a488b79b3f21f1737e71ffe58276d57221180c
Augie <ward31513@gmail.com>
2026-02-18 12:05:22 -0600

    JUnit Testing and Example Set up

---
+++ erfinder/erfinder-server/src/test/java/edu/uiowa/team7/ERFinderApplicationTests.java
@@ -0,0 +1,12 @@
+package edu.uiowa.team7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERFinderApplicationTests {
+    @Test
+    void contextLoads() {
+        // If the database connection in application.properties is wrong, this test will fail.
+    }
+}
